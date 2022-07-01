6b37b7e2abefc7f0dbcc6ad7ee12867d2e2ce208
dbs261 <63139390+dbs261@users.noreply.github.com>
2022-07-01 18:44:48 +0530

    Add files via upload

--- LR_tb.v
+++ LR_tb.v
@@ -21,41 +21,68 @@
 
 
 module LR_tb();
+parameter ADDR_WIDTH = 3, MAX_FEATURES = 7, MAX_DATA_WIDTH = 16*(MAX_FEATURES+1), NUM_DP = 6,
+NUM_FEATURES = 6, NUM_DATA_WIDTH = 16*(NUM_FEATURES+1); //16*6 bit storage
 
-//wire[15:0] xyzw[3:0];
-//wire[15:0] xyz;
-//LR ll(xyz);
-wire CLK;
-//reg[15:0] A, B;
-//wire[15:0] P;
-
-//initial
-//begin
-//CLK = 0;
-//forever
-//    #1 CLK = ~CLK;
-//end
-
-LR lr1(CLK);
-//mult_gen_0 your_instance_name (
-//  .CLK(CLK),  // input wire CLK
-//  .A(A),      // input wire [15 : 0] A
-//  .B(B),      // input wire [15 : 0] B
-//  .P(P)      // output wire [15 : 0] P
-//);
+reg ser, CLK;
+reg[MAX_DATA_WIDTH-1:0] full_data[NUM_DP-1+1:0];
+wire done;
+reg[6:0] counter;//256 bit counter
+reg [ADDR_WIDTH-1:0] addr_reg;
 
 initial
 begin
+    $readmemh("E:/Dhanush/Minor_project/Minor_project.srcs/sources_1/new/data_points.txt",full_data);
+//        fd = $fopen("E:/Dhanush/Minor_project/Minor_project.srcs/sources_1/new/data_points.mem", "r");
+    
+    ser<=full_data[0][NUM_DATA_WIDTH-1];
+//    done<=0;
+    counter<=NUM_DATA_WIDTH-2;
+    addr_reg<=0;
+    forever
+    begin
+        @(posedge CLK)
+        begin
+            if(done==0)
+            begin
+                ser<=full_data[addr_reg][counter];
+            end
+            else
+            begin
+                ser<=0;
+            end
+//            if((addr_reg == NUM_DP)&&(counter == 0))
+//            begin
+//                done<=1;
+//            end
+            if(counter==0)
+            begin
+                counter<=NUM_DATA_WIDTH-1;
+                addr_reg<=addr_reg+1;
+            end
+            else
+            begin
+                counter<=counter-1;
+            end
+        end
+    end
+end
 
-//A = 16'd0; B = 16'd3;
-//#1 A = 16'd4;
-//#2 A = -16'd4;
-//#2 B = -16'd2;
-//#2 A = 16'd3;
+initial
+begin
+CLK = 0;
+forever
+    #1 CLK = ~CLK;
+end
+
+//LR lr1(CLK);
+//assign done = 0;
+main m(CLK, ser, done);
 
 
-
-#20 $finish;
+initial
+begin
+#2000 $finish;
 end
 
 endmodule
